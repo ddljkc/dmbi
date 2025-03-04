7b4f1ad7267f1aa0367d0107341f7fb9778d2ac5
ddljkc <clarkent489@proton.me>
2025-03-04 19:27:58 +0530

    Add

---
+++ prac10.R
@@ -0,0 +1,6 @@
+library(party)
+input.dat <- readingSkills[c(1:105),]
+png(file="decissiontree.png")
+output.tree <- ctree(nativeSpeaker ~ age + shoeSize + score, data= input.dat)	
+dev.off()
+plot(output.tree)
